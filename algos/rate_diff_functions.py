d1937408b0b86f7350ba73968cb42782f3c2275d
Michal <misoberen@gmail.com>
2022-12-05 21:11:38 +0100

    Fix rate diff function

--- algos/rate_diff_functions.py
+++ algos/rate_diff_functions.py
@@ -1,68 +1,70 @@
 """
-Functions used in the stage 1 of the BlockRankingAlgorithm class ranking procedure - obtaining rank difference
-(rank-diff) for each game in the dataset. (They are not intended to be called directly)
-During the ranking procedure, get_rank_diff wrapper function is called and it will call desired rank-diff function.
-Other, specific rank-diff functions should have format r_diff = rank_diff_func(score_w, score_l, **kwargs).
+Functions used in the stage 1 of the BlockRankingAlgorithm class ranking procedure - obtaining rate difference
+(rate-diff) for each game in the Games Dataset (they are not intended to be called directly).
+During the ranking procedure, get_rate_diff wrapper function is called and it will call desired rate-diff function.
+Other, specific rate-diff functions should have format r_diff = rate_diff_func(score_w, score_l, **kwargs).
 """
+import typing as t
 
 import numpy as np
+import pandas as pd
 
 
-# --------------------------------
-# MAIN FUNCTION
-# --------------------------------
-
-def get_rank_diff(opt, rw, **kwargs):
+def get_rate_diff(option: t.Union[str, t.Callable], game_row: pd.Series, **kwargs) -> float:
     """
-    Wrapper function to apply specific rank-diff function ('opt') on the given row ('rw') in the Games dataset.
-    Option 'opt' can be either string or function reference or basically any identifier that is defined here.
-    Scores are read from rw, theoretically also some other info can be obtained from rw.
-    **kwargs are specified in {GamesDataset}.rank_diff_params.
-    If new rank-diff function is specified, it has to be also added here.
+    Wrapper function to apply specific rate-diff function defined by "option" on the given "game row" from the
+    Games Table.
+    
+    :param option: Identifier of the rate-diff function
+    :param game_row: Row from the Games Table
+    :param kwargs: Additional arguments to the rate-diff function (besides score_w and score_l)
+    :return: Rate difference for the given game
     """
-    score_w, score_l = rw['Score_1'], rw['Score_2']
-    if opt in ['win_lose', win_lose_rank_diff_function]:
-        r_diff = win_lose_rank_diff_function(score_w, score_l)
-    elif opt in ['score_diff', 'windmill', score_diff_rank_diff_function]:
-        r_diff = score_diff_rank_diff_function(score_w, score_l)
-    elif opt in ['usau', usau_rank_diff_function]:
-        r_diff = usau_rank_diff_function(score_w, score_l)
+    score_w, score_l = game_row["Score_1"], game_row["Score_2"]
+    if option in ["win_lose", win_lose_rate_diff_function]:
+        r_diff = win_lose_rate_diff_function(score_w, score_l)
+    elif option in ["score_diff", "windmill", score_diff_rate_diff_function]:
+        r_diff = score_diff_rate_diff_function(score_w, score_l)
+    elif option in ["usau", usau_rate_diff_function]:
+        r_diff = usau_rate_diff_function(score_w, score_l)
     else:
-        raise ValueError("Unknown rank-diff option, see algos/rank_diff_functions.py for more info.")
-
+        raise ValueError("Unknown rate-diff option, make sure it is defined in algos/rate_diff_functions.py.")
     return r_diff
 
 
-# --------------------------------
-# INDIVIDUAL FUNCTIONS
-# --------------------------------
-
-def win_lose_rank_diff_function(score_w, score_l):
+def win_lose_rate_diff_function(score_w: int, score_l: int) -> float:
     """
-    Always 1 for the winner (unless it is draw), regardless of the score.
+    Always 1 for the winner (unless it is a draw), regardless of the score.
+
+    :param score_w: Winning score
+    :param score_l: Losing score
+    :return: Game rate difference
     """
-    r_diff = np.sign(score_w - score_l)
-
-    return r_diff
+    return np.sign(score_w - score_l)
 
 
-def score_diff_rank_diff_function(score_w, score_l):
+def score_diff_rate_diff_function(score_w: int, score_l: int) -> float:
     """
     Basic score difference, used in Windmill algo.
+
+    :param score_w: Winning score
+    :param score_l: Losing score
+    :return: Game rate difference
     """
-    r_diff = score_w - score_l
-
-    return r_diff
+    return score_w - score_l
 
 
-def usau_rank_diff_function(score_w, score_l):
+def usau_rate_diff_function(score_w: int, score_l: int) -> float:
     """
-    USAU rank-diff function, source https://play.usaultimate.org/teams/events/rankings/.
+    USAU rate-diff function, source https://play.usaultimate.org/teams/events/rankings/.
+
+    :param score_w: Winning score
+    :param score_l: Losing score
+    :return: Game rate difference
     """
     if score_w == score_l:
         r_diff = 0
     else:
         r = score_l / (score_w - 1) if score_w != 1 else 1
         r_diff = 125 + 475 * (np.sin(np.min([1, 2 * (1 - r)]) * 0.4 * np.pi) / np.sin(0.4 * np.pi))
-
     return r_diff
