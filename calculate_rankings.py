40591c2e1a19bd55456d37298e9fbf207e12b53f
lucas <lucasj@alumni.usc.edu>
2024-02-03 11:59:58 +0100

    separate args and functional concerns of calculate_rankings

--- calculate_rankings.py
+++ calculate_rankings.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import pickle
+import os
 from pathlib import Path
 
 from classes.games_dataset import GamesDataset
@@ -10,6 +11,30 @@ from utils.logging import setup_logger
 
 
 def main():
+
+    parser = argparse.ArgumentParser(description="Parser for EUF ranking calculation.")
+    parser.add_argument(
+        "--input", required=True, type=Path, help="Folder containing the CSV with Games Table (from prepare_data)"
+    )
+    parser.add_argument("--season", required=True, type=int, help="Current year (for naming purposes)")
+    parser.add_argument(
+        "--division", default="all", choices=["women", "mixed", "open", "all"], help="Division (women/mixed/open/all)"
+    )
+    parser.add_argument("--date", required=True, help="Date of calculation")
+    parser.add_argument("--output", required=True, type=Path, help="Path to the folder to save the output CSVs")
+    args = parser.parse_args()
+
+    date_str = args.date.replace("-", "")
+
+    if args.division == "all":
+        divisions = DIVISIONS  # Calculate rankings for all divisions at once
+    else:
+        divisions = [args.division]
+
+    calculate_rankings(args.input, args.season, divisions, date_str, args.output)
+
+
+def calculate_rankings(input_path, season, divisions, date_str, output_path):
     """
     Calculate the rankings for the given division and algorithm.
 
@@ -26,32 +51,18 @@ def main():
     Outputs:
     * CSV with Games, Summary (including ratings)
     """
-    parser = argparse.ArgumentParser(description="Parser for ranking calculation.")
-    parser.add_argument(
-        "--input", required=True, type=Path, help="Folder containing the CSV with Games Table (from prepare_data)"
-    )
-    parser.add_argument("--season", required=True, type=int, help="Current year (for naming purposes)")
-    parser.add_argument(
-        "--division", default="all", choices=["women", "mixed", "open", "all"], help="Division (women/mixed/open/all)"
-    )
-    parser.add_argument("--date", required=True, help="Date of calculation")
-    parser.add_argument("--output", required=True, type=Path, help="Path to the folder to save the output CSVs")
-    args = parser.parse_args()
 
-    date_str = args.date.replace("-", "")
-    if args.division == "all":
-        divisions = DIVISIONS  # Calculate rankings for all divisions at once
-    else:
-        divisions = [args.division]
 
-    setup_logger(args.output / f"calculate_rankings-{args.season}-{args.division}-{date_str}.log")
+
+    os.makedirs(output_path, exist_ok=True)
+    setup_logger(output_path / f"calculate_rankings-{season}-{'_'.join(divisions)}-{date_str}.log")
     logger = logging.getLogger("ranking.data_preparation")
 
     for division in divisions:
         dataset = GamesDataset(
-            args.input / f"{args.season}-{division}-euf-games.csv",
-            name=f"{args.season}-{division}-euf",
-            date=args.date,
+            input_path / f"{season}-{division}-euf-games.csv",
+            name=f"{season}-{division}-euf",
+            date=date_str,
         )
 
         for algo in ALGORITHMS:
@@ -61,9 +72,10 @@ def main():
             rmse, max_sum_resid = get_ranking_metrics(dataset.games, algo.name)
             logger.info(f"RMSE: {rmse:.2f}, Max Sum Resid: {max_sum_resid:.2f}")
 
-        dataset.games.to_csv(args.output / f"{dataset.name}-games-{date_str}.csv", index=False, float_format="%.3f")
-        dataset.summary.to_csv(args.output / f"{dataset.name}-summary-{date_str}.csv", float_format="%.3f")
-        logger.info(f"Output files saved to {args.output}.")
+
+        dataset.games.to_csv(output_path / f"{dataset.name}-games-{date_str}.csv", index=False, float_format="%.3f")
+        dataset.summary.to_csv(output_path / f"{dataset.name}-summary-{date_str}.csv", float_format="%.3f")
+        logger.info(f"Output files saved to {output_path}.")
 
 
 if __name__ == "__main__":
