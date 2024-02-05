770846fe498db7903f0ed2339c8fd6750955dffa
lucas <lucasj@alumni.usc.edu>
2024-02-05 19:15:20 +0100

    get rid of all args stuff and main() in calculate_rankings

--- calculate_rankings.py
+++ calculate_rankings.py
@@ -9,43 +9,6 @@ from utils.dataset import get_ranking_metrics
 from utils.logging import setup_logger
 
 
-def main():
-    """
-    Calculate the rankings for the given division.
-
-    Prerequisites:
-    * Run prepare_data.py script (use its output path as input to this script)
-
-    Arguments:
-    * --input - path to the folder with all necessary files
-    * --season - current year
-    * --division - women/mixed/open/all
-    * --date - date of calculation
-    * --output - path to the folder in which to save the output files
-
-    Outputs:
-    * CSV with Games, Summary (including ratings)
-    """
-    parser = argparse.ArgumentParser(description="Parser for EUF ranking calculation.")
-    parser.add_argument(
-        "--input", required=True, type=Path, help="Folder containing the CSV with Games Table (from prepare_data)"
-    )
-    parser.add_argument("--season", required=True, type=int, help="Current year (for naming purposes)")
-    parser.add_argument(
-        "--division", default="all", choices=["women", "mixed", "open", "all"], help="Division (women/mixed/open/all)"
-    )
-    parser.add_argument("--date", required=True, help="Date of calculation")
-    parser.add_argument("--output", required=True, type=Path, help="Path to the folder to save the output CSVs")
-    args = parser.parse_args()
-
-    if args.division == "all":
-        divisions = DIVISIONS  # Calculate rankings for all divisions at once
-    else:
-        divisions = [args.division]
-
-    calculate_rankings(args.input, args.season, divisions, args.date, args.output)
-
-
 def calculate_rankings(input_path: Path, season: int, divisions: [str], date: str, output_path: Path):
     """
     Perform the calculation of the rankings for the given division(s) and input.
@@ -62,9 +25,9 @@ def calculate_rankings(input_path: Path, season: int, divisions: [str], date: st
     :date_str: date of calculation
     :output_path: path to the folder in which to save the output files
     """
-    date_str = args.date.replace("-", "")
+    date_str = date.replace("-", "")
     output_path.mkdir(exist_ok=True, parents=True)
-    
+
     setup_logger(output_path / f"calculate_rankings-{season}-{'_'.join(divisions)}-{date_str}.log")
     logger = logging.getLogger("ranking.data_preparation")
 
@@ -86,7 +49,3 @@ def calculate_rankings(input_path: Path, season: int, divisions: [str], date: st
         dataset.games.to_csv(output_path / f"{dataset.name}-games-{date_str}.csv", index=False, float_format="%.3f")
         dataset.summary.to_csv(output_path / f"{dataset.name}-summary-{date_str}.csv", float_format="%.3f")
         logger.info(f"Output files saved to {output_path}.")
-
-
-if __name__ == "__main__":
-    main()
